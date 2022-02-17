7f43dc71066f09f6b33a42c1c89f3f3ca5bd33e9
LeoAnyu <1115495923@qq.com>
2022-02-17 13:17:32 +0800

    MyMessage01_Background_202202171316

---
+++ src/main/java/com/lyy/WebsocketChatroomApplication.java
@@ -0,0 +1,14 @@
+package com.lyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketChatroomApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WebsocketChatroomApplication.class, args);
+    }
+
+}
